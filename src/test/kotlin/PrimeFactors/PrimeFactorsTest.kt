7be4fd670146d3997eb632b6cc01a84d4da3d802
Patrick GIRY <patrickgiry@gmail.com>
2018-02-28 23:40:37 +0100

    2 has 2 as factor

--- src/test/kotlin/PrimeFactors/PrimeFactorsTest.kt
+++ src/test/kotlin/PrimeFactors/PrimeFactorsTest.kt
@@ -12,8 +12,18 @@ class PrimeFactorsTest {
         assertThat(factorOf(1)).isEmpty()
     }
 
+    @Test
+    @DisplayName("2 has 2 as factor")
+    fun factors_of_2() {
+        assertThat(factorOf(2)).containsExactly(2)
+    }
+
     private fun factorOf(n: Int): List<Int> {
-        return emptyList()
+        val factors = mutableListOf<Int>()
+        if (n > 1) {
+            factors.add(2)
+        }
+        return factors
     }
 
 
